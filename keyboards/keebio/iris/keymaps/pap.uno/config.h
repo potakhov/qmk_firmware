cd57ff11eced0ecf731c32a669d00187af75be2b
Alexey Potakhov <alex@potahov.com>
2019-12-04 09:54:59 -0500

    Moving Iris layout to the same branch

---
+++ keyboards/keebio/iris/keymaps/pap.uno/config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define EE_HANDS
+
+#undef RGBLED_NUM
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 12
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+
+#define NO_ACTION_ONESHOT
+#define NO_ACTION_MACRO
+
+#undef PRODUCT
+#define PRODUCT         "Keebio Iris pap.uno 0.8"
