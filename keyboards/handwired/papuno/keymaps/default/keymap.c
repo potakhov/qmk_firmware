2d194200b9bf992b48dbddbe190f105f149e001c
Alexey Potakhov <alex@potahov.com>
2020-01-15 14:25:08 -0500

    changing the layout... again

--- keyboards/handwired/papuno/keymaps/default/keymap.c
+++ keyboards/handwired/papuno/keymaps/default/keymap.c
@@ -2,11 +2,7 @@
 
 #define KC_          KC_TRNS
 #define KC_LGRV      LT(2,KC_GRV)
-#define KC_TBSH      LSFT_T(KC_TAB)
-#define KC_ALSP      LALT_T(KC_SPC)
 #define KC_LUPR      MO(1)
-#define KC_LSYS      MO(2)
-#define KC_GUES      LGUI_T(KC_ESC)
 #define KC_GUIQ      LGUI(KC_Q)
 #define KC_GUIS      LGUI(KC_S)
 #define KC_GUIW      LGUI(KC_W)
@@ -17,14 +13,11 @@
 #define KC_GUIT      LGUI(KC_T)
 #define KC_GUIA      LGUI(KC_A)
 #define KC_GGRV      LGUI(KC_GRV)
-#define KC_GITB      LGUI(KC_TAB)
-#define KC_GISP      LGUI(KC_SPC)
+#define KC_GUIF      LGUI(KC_F)
 #define KC_SFQU      LSFT(KC_QUOT)
 #define KC_MOSX      MAGIC_UNSWAP_CTL_GUI
 #define KC_MWIN      MAGIC_SWAP_CTL_GUI
-#define KC_SLBR      LSFT(KC_LBRC)
-#define KC_SRBR      LSFT(KC_RBRC)
-#define KC_SSCL      LSFT(KC_SCLN)
+#define KC_SEQL      LSFT(KC_EQL)
 #define KC_TGBL      BL_TOGG
 #define KC_STBL      BL_STEP
 #define KC_RST       RESET
@@ -38,11 +31,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
       TAB,  Q,   W,   E,   R,   T,                  Y,   U,   I,   O,   P, BSLS,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     TBSH,  A,   S,   D,   F,   G,                  H,   J,   K,   L, SCLN,QUOT,
+      TAB,  A,   S,   D,   F,   G,                  H,   J,   K,   L, SCLN,QUOT,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     LSFT,  Z,   X,   C,   V,   B,                  N,   M, COMM, DOT,SLSH,RSFT,
+     LSFT,  Z,   X,   C,   V,   B,                  N,   M, COMM, DOT,SLSH,SFTENT,
   //`----+----+----+----+----+----|              |----+----+----+----+----+----'
-          SLBR,SRBR, ESC,LUPR, SPC,               BSPC,LUPR, ENT,LBRC,RBRC,
+          SEQL, EQL, ESC,LUPR, SPC,               BSPC,LUPR, ENT,LBRC,RBRC,
   //`----+----+----+----+----+----|              |----+----+----+----+----+----'
                          LALT,LGUI,               RGUI,RCTL
   //`----+----+----+----+----+----|              |----+----+----+----+----+----'
@@ -52,11 +45,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
      GGRV, F1,  F2,  F3,  F4,  F5,                 F6,  F7,  F8,  F9, F10, BSPC,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     GITB,GUIQ,GUIW,    ,PGUP,GUIT,                EQL,HOME,  UP, END,PSCR,PAUS,
+         ,GUIQ,GUIW,PGUP,    ,GUIT,                   ,HOME,  UP, END,PSCR,PAUS,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-      TAB,GUIA,GUIS,    ,PGDN,    ,               SFQU,LEFT,DOWN,RGHT,QUOT,    ,
+         ,GUIA,GUIS,PGDN,GUIF,    ,                   ,LEFT,DOWN,RGHT,    ,    ,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         ,GUIZ,GUIX,GUIC,GUIV,    ,               SLBR,SRBR,LBRC,RBRC,    ,    ,
+         ,GUIZ,GUIX,GUIC,GUIV,    ,                   ,    ,    ,    ,    ,    ,
   //`----+----+----+----+----+----|              |----+----+----+----+----+----'
               ,    ,    ,    ,    ,                   ,    ,    ,    ,    ,
   //`----+----+----+----+----+----|              |----+----+----+----+----+----'
@@ -66,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [2] = LAYOUT_kc(
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-         ,MOSX,MWIN, USB,  BT,    ,                   ,    ,    ,    ,    ,    ,
+         ,MOSX,MWIN,  BT, USB,    ,                   ,    ,    ,    ,    ,    ,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
          ,    ,    ,    ,    ,    ,                   ,    ,    ,    ,    ,    ,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
